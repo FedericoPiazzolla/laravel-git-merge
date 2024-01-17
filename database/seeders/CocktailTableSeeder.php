3c7f13d7451608bdcae928cb39ca585bd687f729
danieldorazio <danieldorazio.97@gmail.com>
2024-01-17 17:28:55 +0100

    update seeder function with api cocktail

--- database/seeders/CocktailTableSeeder.php
+++ database/seeders/CocktailTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\Cocktail;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Faker\Generator as Faker;
+use GuzzleHttp\Client;
 
 class CocktailTableSeeder extends Seeder
 {
@@ -14,7 +14,7 @@ class CocktailTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Faker $faker)
+    public function run(): void
     {
         //
         // $cocktail = new Cocktail();
@@ -24,19 +24,44 @@ class CocktailTableSeeder extends Seeder
         // $cocktail->is_alcolic = true;
         // $cocktail->save();
 
-        $drink_name =['mojito', 'negroni' , 'spritz' ,'long island'];
-        $drink_type = ['muddled', 'build', 'shake', 'stir and strain'];
-        $drink_price = [5.00, 10.00 , 8.00, 12.00];
+        // $drink_name =['mojito', 'negroni' , 'spritz' ,'long island'];
+        // $drink_type = ['muddled', 'build', 'shake', 'stir and strain'];
+        // $drink_price = [5.00, 10.00 , 8.00, 12.00];
         
-        for($i=0; $i < 20; $i++ ){
-        $cocktail = new Cocktail();
-        $cocktail->name = $faker->randomElement($drink_name);
-        $cocktail->price =$faker->randomElement($drink_price);
-        $cocktail->type = $faker->randomElement($drink_type);
-        $cocktail->is_alcolic = $faker->boolean();
-       
-        $cocktail->save();
-        };
+        // for($i=0; $i < 20; $i++ ){
+        // $cocktail = new Cocktail();
+        // $cocktail->name = $faker->randomElement($drink_name);
+        // $cocktail->price =$faker->randomElement($drink_price);
+        // $cocktail->type = $faker->randomElement($drink_type);
+        // $cocktail->is_alcolic = $faker->boolean();
+        // $cocktail->save();
+        // };
 
+        $rows = $this->getData(2);
+        
+        foreach ($rows as $row) {
+            $cocktail               = new Cocktail(); 
+            $cocktail->name         = $row->strDrink;
+            $cocktail->glass        = $row->strGlass;
+            $cocktail->instruction  = $row->strInstructionsIT;
+            $cocktail->image        = $row->strDrinkThumb;
+            $cocktail->is_alcoholic = $row->strAlcoholic;
+
+            $cocktail->save();
+        }
+    }
+
+    protected function getData(int $total = 10)
+    {
+        $rows   = [];
+        $url    = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+        $client = new Client(['verify' => false]);
+
+        for ($i = 0; $i < $total; $i++) {
+            $response = $client->get($url);
+            $row      = json_decode($response->getBody());
+            $rows     = [...$rows, ...$row->drinks];
+        }
+        return $rows;
     }
 }
