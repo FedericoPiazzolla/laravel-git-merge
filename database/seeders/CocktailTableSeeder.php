6a6b7aadfd0acf66601ab4704b045a7813f98cc1
lexbert17 <lexbertlazo@gmail.com>
2024-01-17 16:25:00 +0100

    add:  faker_item

--- database/seeders/CocktailTableSeeder.php
+++ database/seeders/CocktailTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Cocktail;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class CocktailTableSeeder extends Seeder
 {
@@ -13,15 +14,29 @@ class CocktailTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //
-        $cocktail = new Cocktail();
-        $cocktail->name ='Mojito';
-        $cocktail->price =10.00;
-        $cocktail->type ='Muddled';
-        $cocktail->is_alcolic = true;
-        $cocktail->save();
+        // $cocktail = new Cocktail();
+        // $cocktail->name ='Mojito';
+        // $cocktail->price =10.00;
+        // $cocktail->type ='Muddled';
+        // $cocktail->is_alcolic = true;
+        // $cocktail->save();
+
+        $drink_name =['mojito', 'negroni' , 'spritz' ,'long island'];
+        $drink_type = ['muddled', 'build', 'shake', 'stir and strain'];
+        $drink_price = [5.00, 10.00 , 8.00, 12.00];
         
+        for($i=0; $i < 20; $i++ ){
+        $cocktail = new Cocktail();
+        $cocktail->name = $faker->randomElement($drink_name);
+        $cocktail->price =$faker->randomElement($drink_price);
+        $cocktail->type = $faker->randomElement($drink_type);
+        $cocktail->is_alcolic = $faker->boolean();
+       
+        $cocktail->save();
+        };
+
     }
 }
