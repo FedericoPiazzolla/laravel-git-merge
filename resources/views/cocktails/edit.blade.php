a39700d9f876d00ba2c1e3352176ea268353b2bf
FedericoPiazzolla <Federicop257@gmail.com>
2024-01-31 17:26:44 +0100

    add: refactoring style create, edit, show

--- resources/views/cocktails/edit.blade.php
+++ resources/views/cocktails/edit.blade.php
@@ -2,25 +2,25 @@
 
 
 @section('content')
-    <div class="container">
+    <div class="container my-5">
         <form action="{{ route('cocktails.update', ['cocktail' => $cocktail->slug])}} " method="POST" >
             @csrf
             @method('PUT')
             <div class="mb-3">
-                <label for="name" class="form-label">nome</label>
+                <label for="name" class="form-label">Name</label>
                 <input type="text" class="form-control" id="name" name="name" value="{{ $cocktail->name }}" >
             </div>
 
             <div class="mb-3">
-                <label for="glass" class="form-label">glass</label>
+                <label for="glass" class="form-label">Glass</label>
                 <input type="text" class="form-control" id="glass" name="glass" value="{{ $cocktail->glass }}">
             </div>
 
             <div class="mb-3">
-                <label for="instruction" class="form-label">instruction</label>
+                <label for="instruction" class="form-label">Instruction</label>
                 <textarea class="form-control" id="instruction" name="instruction" rows="3">{{ $cocktail-> instruction}}</textarea>
             </div>
-            <label for="is_alcholic" class="form-label"></label>
+            <label for="is_alcholic" class="form-label">Is alcholic?</label>
             <select class="form-select" aria-label="Default select example" id="is_alcholic" name="is_alcholic">
 
                 <option selected>seleziona</option>
@@ -28,7 +28,7 @@
                 <option @selected($cocktail->is_alcholic !== 'unalcholic') value="unalcholic">unalcholic</option>
 
             </select>
-            <button type="submit" class="btn btn-primary">salva</button>
+            <button type="submit" class="btn btn-success mt-3">salva</button>
         </form>
     </div>
 @endsection
